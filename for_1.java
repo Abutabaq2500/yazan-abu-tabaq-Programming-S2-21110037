e6a1fd27f5eee8fac3fc22d91afb33a1fd3241ee
Abutabaq2500 <109656817+Abutabaq2500@users.noreply.github.com>
2022-07-26 11:15:30 +0300

    Add files via upload

---
+++ for_1.java
@@ -0,0 +1,16 @@
+package In_lecture;
+
+public class for_1 {
+
+	public static void main(String[] args) {
+
+		int plus = 0;
+		for(int count = 0;count <= 50;count++)
+		{
+			plus = plus + count;
+			
+		}
+		System.out.println(plus);
+	}
+
+}
