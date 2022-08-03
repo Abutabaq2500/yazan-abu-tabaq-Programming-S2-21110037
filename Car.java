6f87374edaf7aa029ccccabe5f84a1e916170d8f
Abutabaq2500 <109656817+Abutabaq2500@users.noreply.github.com>
2022-08-03 12:03:54 +0300

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,24 @@
+package In_lecture;
+
+public class Car {
+
+		
+		String model;
+		String color;
+		int year;
+	public static void main(String[] args) {
+
+		
+		method();
+	}
+	static void method() {
+		
+		Car obj = new Car();
+		
+		System.out.println(obj.model);
+		System.out.println(obj.color);
+		System.out.println(obj.year);
+	}
+	
+
+}
---
+++ Car_demo.java
@@ -0,0 +1,14 @@
+package In_lecture;
+
+public class Car_demo {
+
+	public static void main(String[] args) {
+		Car obj = new Car();
+		obj.model = "Nissan";
+		obj.color = "Black";
+		obj.year = 2022;
+		System.out.println(obj.model + " " + obj.color + " " + obj.year);
+
+	}
+
+}
